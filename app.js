76d7d9212def06be9093e7bd0bd5298a6f645dfe
NelliBilovol <nelli199020@gmail.com>
2022-04-02 18:31:33 +0300

    start work with swagger

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -9,7 +11,7 @@ const transactionsRouter = require('./routes/api/transactions');
 const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
