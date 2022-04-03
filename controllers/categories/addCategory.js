4ea1ccfa6ba7e52a6d1505620297c64d18125530
NelliBilovol <nelli199020@gmail.com>
2022-04-03 21:13:55 +0300

    add swagger of categories

--- controllers/categories/addCategory.js
+++ controllers/categories/addCategory.js
@@ -2,7 +2,7 @@ const { Category } = require('../../models');
 
 const addCategory = async (req, res) => {
   const { _id } = req.user;
-  const result = await Category.create({ ...req.body, owner: _id });
+  const { result } = await Category.create({ ...req.body, owner: _id });
 
   res.status(201).json({
     status: 'success',
