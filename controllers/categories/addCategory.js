b67a6a9aabac77c711c0297ba8a7ef7de0031926
NelliBilovol <nelli199020@gmail.com>
2022-04-03 21:18:42 +0300

    fiex response of addCategory

--- controllers/categories/addCategory.js
+++ controllers/categories/addCategory.js
@@ -2,15 +2,9 @@ const { Category } = require('../../models');
 
 const addCategory = async (req, res) => {
   const { _id } = req.user;
-  const { result } = await Category.create({ ...req.body, owner: _id });
+  const result = await Category.create({ ...req.body, owner: _id });
 
-  res.status(201).json({
-    status: 'success',
-    code: 201,
-    data: {
-      result,
-    },
-  });
+  res.status(201).json(result);
 };
 
 module.exports = addCategory;
