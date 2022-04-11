134af12f1c8e485776f81be2399c554ba77f7345
Artem Bilodid <ar.bilodid@draftkings.com>
2022-04-11 20:54:56 +0300

    implemented pagination for Transactions

--- controllers/transactions/getTransactions.js
+++ controllers/transactions/getTransactions.js
@@ -3,11 +3,14 @@ const { Transaction } = require('../../models');
 const getTransactions = async (req, res, next) => {
   try {
     const { _id } = req.user;
-    const TRANSACTIONS_LIMIT = 5;
+    const { page = 1, size = 5 } = req.query;
+
+    const skip = (Number(page) - 1) * size;
 
     const transactions = await Transaction.find({ owner: _id })
       ?.sort({ createdAt: -1 })
-      ?.limit(TRANSACTIONS_LIMIT)
+      ?.skip(skip)
+      ?.limit(Number(size))
       ?.populate('category', 'name');
 
     return res.status(200).json(transactions);
